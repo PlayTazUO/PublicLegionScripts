f0a1f7b7e41b7d3ead5a744c1d206db93659b5b8
Tad Taylor <bittiez@gmail.com>
2025-10-27 19:56:03 -0600

    Improve forge targeting in Smelt.py
    Refactor smelting script to check for nearby forges before targeting.

--- Resources/UOMemento/Smelt.py
+++ Resources/UOMemento/Smelt.py
@@ -2,23 +2,33 @@
 #   https://github.com/PlayTazUO/PublicLegionScripts/
 # Or documentation at
 #   https://github.com/PlayTazUO/TazUO/wiki/TazUO.Legion-Scripting
-
-# A simple script to smelt ores at a forge
-
 import API
 
 ORES = [6585]
+FORGES = [0x0FB1, 0x197E, 0x1982, 0x198A, 0x198E]
 
-API.HeadMsg("TARGET A FORGE", API.Player, 32)
-Forge = API.RequestAnyTarget()
+Forge = None
+px = API.Player.X
+py = API.Player.Y
+
+stats = API.GetStaticsInArea(px - 2, py - 2, px + 2, py + 2)
+
+if stats:
+    for static in stats:
+        if static.Graphic in FORGES:
+            Forge = static
+            break
+
+if not Forge:
+    API.HeadMsg("TARGET A FORGE", API.Player, 32)
+    Forge = API.RequestAnyTarget()
 
 if not Forge:
     API.HeadMsg("NO FORGE TARGETED", API.Player, 32)
     API.Stop()
 
 for ore in ORES:
-    while API.FindType(ore):
+    if API.FindType(ore):
         API.UseObject(API.Found)
         API.WaitForTarget()
         API.Target(Forge.X, Forge.Y, Forge.Z, Forge.Graphic)
-        API.Pause(0.8)
